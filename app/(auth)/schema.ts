5fdff899f968c5fc13f0a6fc54e5719b5e3977b0
Shreyash-Adhikari1 <adhikarishreyash@gmail.com>
2026-01-19 20:06:20 +0545

    username field added to frotnend zod schema

--- app/(auth)/schema.ts
+++ app/(auth)/schema.ts
@@ -1,18 +1,21 @@
 import { z } from "zod";
 
-export const registerSchema = z.object({
+export const registerSchema = z
+  .object({
     fullName: z.string().min(2, "Name is required"),
+    username: z.string().min(5, "Username is required"),
     email: z.string().email("Invalid email address"),
     phoneNumber: z.string().min(10, "Invalid phone number"),
     address: z.string().min(5, "Address is required"),
     password: z.string().min(8, "Password must be at least 8 characters"),
-    confirmPassword: z.string()
-}).refine((data) => data.password === data.confirmPassword, {
+    confirmPassword: z.string(),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
     message: "Passwords don't match",
     path: ["confirmPassword"],
-});
+  });
 
 export const loginSchema = z.object({
-    email: z.string().email(),
-    password: z.string().min(1, "Password is required"),
+  email: z.string().email(),
+  password: z.string().min(1, "Password is required"),
 });
