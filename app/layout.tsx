7448c90fb1b81dbbbcd311b7c2afa445b3d9b1a6
Shreyash-Adhikari1 <adhikarishreyash@gmail.com>
2026-02-01 00:28:11 +0545

    Cookie SetToken bit updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import "./globals.css";
-// import { AuthProvider } from "@/context/AuthContext";
 
 export const metadata: Metadata = {
   title: "ArtSphere",
@@ -14,10 +13,31 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className="antialiased font-serif">
-        {/* <AuthProvider>{children}</AuthProvider> */}
-        {children}
-      </body>
+      <body className="antialiased font-serif">{children}</body>
     </html>
   );
 }
+
+// import type { Metadata } from "next";
+// import "./globals.css";
+// // import { AuthProvider } from "@/context/AuthContext";
+
+// export const metadata: Metadata = {
+//   title: "ArtSphere",
+//   description: "A safe space for student artists",
+// };
+
+// export default function RootLayout({
+//   children,
+// }: {
+//   children: React.ReactNode;
+// }) {
+//   return (
+//     <html lang="en">
+//       <body className="antialiased font-serif">
+//         {/* <AuthProvider>{children}</AuthProvider> */}
+//         {children}
+//       </body>
+//     </html>
+//   );
+// }
